b7b6558396a9bc640e58a25bab5ca85550b3bee4
duero <dueromoods@gmail.com>
2023-09-15 08:52:42 -0900

    Updated formatting and moved more into secrets

--- Birthdays.py
+++ Birthdays.py
@@ -1,27 +1,30 @@
 import re
-import os.path
 import smtplib
 import ssl
 import sys
-from importFiles import decryptFiles, updateSecrets
+from datetime import datetime
+from importFiles import updateZip
 from parseWebsite import gatherWebsite
+import mySecretsFile as msf
 
 
-login = open("Login", "r").read().splitlines()
+isHeadless = sys.argv == "HEADLESS"
 
 
-def Quit():
-    print("Canceling...")
+def Quit(input = None):
+    print(input) if input != None else print("Canceling...")
     sys.exit()
 
 
-def convertToEmails(inputE:list[str]):
-    emailsList:list[str] = []
+def convertToEmails(inputE: list[str]):
+    emailsList: list[str] = []
     for person in inputE:
-        person = str(person).replace(" ", "").lower() + "@danville.k12.in.us"
+        # add mailing domain then check if is valid
+        person = str(person).replace(" ", "").lower() + msf.var2
         if re.match(r"(mr)", str(person)) != "None" and re.match(r"(mrs)", str(person)) != "None" and re.match(r"(ms)", str(person)) != "None" and re.findall(r"(no)", person) == []:
             emailsList.append(person)
         else:
+            # if not valid, ask for user input
             response = input(
                 f"\nPerson is: {person}, please type in email address\n")
             if response != "":
@@ -31,26 +34,38 @@ def convertToEmails(inputE:list[str]):
     return emailsList
 
 
+def gettoday():
+    for data in msf.database:
+        if (datetime.today().date() - data[0]).days % 365 == 0:  # type: ignore
+            return data[1]
+    return "None"
+
+
 def emailPeople(people: list[str]):
+    if people == "None":
+        Quit()
     if input(f"\nSending to: {people}\nPress enter to continue") != "":
         Quit()
-    username = login[0]
-    password = login[1]
-    port = 587  # For starttl
+    username = msf.var8
+    password = msf.pass1
+    port = 587
     smtp_server = "smtp.gmail.com"
-    message = open("messages").read()
     context = ssl.create_default_context()
-    with smtplib.SMTP(smtp_server, port) as server:
-        server.starttls(context=context)
-        server.login(username, password)
-        server.sendmail(username, people, message)
+
+    server = smtplib.SMTP(smtp_server, port)
+    server.starttls(context=context)
+    server.login(username, password)
+    server.sendmail(username, people, msf.message)
+    server.close()
 
 
 # Where the code actually runs
 if __name__ == "__main__":
-    if not os.path.exists("Database.txt"):
-        if not os.path.isdir("emlFiles"):
-            decryptFiles()
-    # check for argv, if exists, update db with link, if not, run headless
-    emailPeople(convertToEmails(gatherWebsite()))
-    updateSecrets()
+    updateZip()
+    if isHeadless:
+        emails:list[str] = gettoday() # type: ignore
+    else:
+        # update database
+        emails = convertToEmails(gatherWebsite())
+    emailPeople(emails)
+    updateZip()
--- importFiles.py
+++ importFiles.py
@@ -5,67 +5,84 @@ import os.path
 from datetime import datetime
 import pyzipper
 from zipfile import ZipFile
+try:
+    import mySecretsFile as msf
+except:
+    raise FileNotFoundError(
+        "You need to have the secrets file to access this program!!")
+
 
 regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
 
 
-def getPass():
-    return open("Login").readlines()[1].strip().join(open("Login").readlines()[3].strip()).encode('utf-8')
+# for original parsing of .eml files || left in for sake of studying
+def _getPass():
+    return msf.pass1.strip().join(msf.pass2.strip()).encode('utf-8')
 
 
-def getFiles():
+def _getFiles():
     folder = "emlFiles"
     return [email.message_from_bytes(open(file, "rb").read()) for file in [f"{folder}/{subdir}/{file}" for subdir in os.listdir(f"{folder}") for file in os.listdir(f"{folder}/{subdir}")]]
 
 
-def stripEmailsFiles():
+def _stripEmailsFiles():
     output = ""
-    for daysEmail in getFiles():
+    for daysEmail in _getFiles():
         if not daysEmail["Bcc"] and not daysEmail["To"]:
             continue
-        allAddresses = re.findall(regex, (daysEmail["Bcc"] + daysEmail["To"] if daysEmail["Bcc"]
+        allAddresses = re.findall(regex, (daysEmail["Bcc"] + daysEmail["To"] if daysEmail["Bcc"] 
                                   and daysEmail["To"] else daysEmail["Bcc"] if daysEmail["Bcc"] else daysEmail["To"]))
         mailDate = daysEmail["Date"]
 
-        if "REDACTED" not in allAddresses:  # put in personal email address
+        if msf.var8 not in allAddresses: 
             output += str([str(datetime.strptime(str(mailDate).replace(" (PDT)", ""), "%a, %d %b %Y %H:%M:%S %z").date()),
                           str(json.dumps(allAddresses))]).replace("\'", "\"") + "\n"  # type: ignore
     return output
 
 
-def decryptFiles():
-    if not os.path.exists("Database.txt"):
+# for reading Database.txt
+# left encrypted because I shouldn't really put that on github
+
+def _unZip():
+    print("Unzipping files...", end="")
+    pyzipper.AESZipFile("Secrets.zip", "r", compression=pyzipper.ZIP_LZMA,
+                        encryption=pyzipper.WZ_AES).extractall(pwd=_getPass())
+    print(" done!")
+
+
+def _decryptFiles():
+    if not os.path.exists("mySecretsFile.py"):
         if os.path.exists("Secrets.zip"):
-            pyzipper.AESZipFile("Secrets.zip", "r", compression=pyzipper.ZIP_LZMA,
-                                encryption=pyzipper.WZ_AES).extractall(pwd=getPass())
+            _unZip()
         else:
             if os.path.isdir("emlFiles"):
-                open("Database.txt", "w").write(stripEmailsFiles())
+                open("Database.txt", "w").write(_stripEmailsFiles())
                 print("Saved as Database.txt")
+            elif os.path.exists("emlFiles.zip"):
+                ZipFile("emlFiles.zip").extractall("emlFiles", pwd=_getPass())
+                _decryptFiles()
             else:
-                ZipFile("emlFiles.zip").extractall("emlFiles", pwd=getPass())
-                decryptFiles()
+                raise PermissionError(
+                    "Haha, you tried to access things that you don't have access to!!")
+    else:
+        if not hasattr(msf, "saveMe") or not bool(msf.saveMe):
+            _unZip()
 
 
-def updateSecrets():
-    try:
-        os.remove("Secrets.zip")
-    except:
-        1 == 1
+def _updateSecrets():
+    
+    os.remove("Secrets.zip") if os.path.exists("Secrets.zip") else None
     zf = pyzipper.AESZipFile(
         "Secrets.zip", "x", compression=pyzipper.ZIP_LZMA, encryption=pyzipper.WZ_AES)
-    zf.setpassword(getPass())
-    # zf.write("Login")
-    zf.write("Database.txt")
-    zf.write("message")
-    zf.write("emlFiles.zip") if os.path.exists("emlfiles.zip") else 1 == 1
+    zf.setpassword(_getPass())
+    zf.write("mySecretsFile.py")
     zf.close()
 
 
-def runAll():
-    decryptFiles()
-    updateSecrets()
+def updateZip():
+    _decryptFiles()
+    _updateSecrets()
 
 
 if __name__ == "__main__":
-    runAll()
+    updateZip()
